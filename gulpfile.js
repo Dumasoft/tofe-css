584102486c12637e528dc72f533f5cb9699aa512
brunogomezgarcia <bronigege@gmail.com>
2019-02-09 13:11:03 +0100

    colores y nueva configuración de gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,37 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var concat = require('gulp-concat-css');
-var limpiarcss = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var cssmin = require('cssnano');
+var prefix = require('autoprefixer');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var sassLint = require('gulp-sass-lint');
 var sourcemaps = require('gulp-sourcemaps');
+var postcss = require('gulp-postcss');
 var origen = 'sass/tofe.sass';
 var destino = 'dist/css';
+var opcionesPrefix = {
+    browsers: ['last 2 versions']
+}
 
-gulp.task('sass', function() {
-    gulp.src(origen)
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(destino))
-});
+function estilo() {
+    return (
+        gulp
+            .src(origen)
+            .pipe(sourcemaps.init())
+            .pipe(sass({outputStyle: 'expanded'}))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest(destino))
+            .pipe(postcss([prefix(opcionesPrefix), cssmin()]))
+            .pipe(rename({suffix: '.min'}))
+            .pipe(gulp.dest(destino))
+    );
+}
 
-gulp.task('sass-minified', function() {
-    gulp.src(origen)
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(concat('tofe.min.css'))
-        .pipe(limpiarcss())
-        .pipe(gulp.dest(destino))
-});
 
-gulp.task('default', ['sass', 'sass-minified']);
+function watch() {
+    gulp.watch(origen, estilo)
+}
+
+exports.estilo = estilo;
+exports.watch = watch;
